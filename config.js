0cc5479a752a14f4b6f8093599f3afb97b23fee1
MaiHuy666 <98572522+MaiHuy666@users.noreply.github.com>
2022-01-28 11:50:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const CONFIG = {
     introTitle: 'Bây bê à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `Dù cho muôn trùng phương,
+    Còn bao nhiêu lời thương
+    Dù mênh mông đại dương,
+    Phai đi sắc hương mơ màng
+    Anh vẫn yêu mình em thôi đấy
+    Yêu em mà chẳng 1 lời thở than :33`,
+    btnIntro: 'Vâng <333',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vâng, em đã say từ cái nhìn đầu tiên <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều tí 😘😘',
-    messDesc: 'Cảm ơn em :3 nào gặp anh mua kẹo cho nhé 😘',
+    reply: 'Bời vì trong mắt em anh đz nhất luôn <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều hơn 1 tí 😘😘',
+    messDesc: 'Cảm ơn em :3 anh mua kẹo cho nhé 😘',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
